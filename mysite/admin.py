d74b70f3eab40309e8232548cc4ca2d1a76eb7c2
gerasimovtltsu <theavg@yandex.ru>
2023-05-09 17:41:11 +0400

    edit admin import

--- mysite/admin.py
+++ mysite/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from project.models import Person, StudentGroup
+from mysite.models import Person, StudentGroup
 
 admin.site.register(Person)
 admin.site.register(StudentGroup)
